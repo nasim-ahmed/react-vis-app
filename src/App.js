1bcf56b4b1d9c6f68f270cc28439d7b4be3dbcc5
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-04-26 13:40:33 +0200

    added pie plots

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Plot from 'react-plotly.js';
 import BoxPlot from './components/BoxPlot';
+import PiePlot from './components/PiePlot';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Box Plot</h1>
-        <BoxPlot />
+      <BoxPlot />
+      <h1>Pie Plot</h1>
+        <PiePlot />
       
     </div>
   );
