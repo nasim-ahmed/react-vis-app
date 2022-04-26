ae65ddba49271b514f1dcda762613d38e3dfdd4c
Nasim Ahmed <nasim.ahmed54@gmail.com>
2022-04-26 13:36:25 +0200

    added box plots

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Plot from 'react-plotly.js';
+import BoxPlot from './components/BoxPlot';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Box Plot</h1>
+        <BoxPlot />
+      
     </div>
   );
 }
